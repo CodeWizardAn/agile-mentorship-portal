77a400522aa14677ace84e3f856719c4e4dde082
CodeWizardAn <codewizardan@gmail.com>
2026-06-09 21:29:53 +0530

    Added PostgreSQL integration, SQLAlchemy models, signup and login functionality

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from aiohttp import request
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi import Form
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from security import hash_password
+from database import get_db
+from models import User
 
 app = FastAPI()
 
@@ -57,3 +64,32 @@ def admin_dashboard(request: Request):
         request=request,
         name="admin_dashboard.html"
     )
+    
+@app.post("/signup/{role}")
+def create_user(
+    role: str,
+    full_name: str = Form(...),
+    email: str = Form(...),
+    password: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(User).filter(User.email == email).first()
+    if existing_user:
+        return HTMLResponse(
+            "<h2>Email already registered</h2>",
+            status_code=400
+    )
+    user = User(
+        full_name=full_name,
+        email=email,
+        password_hash=hash_password(password),
+        role=role.upper(),
+        status="ACTIVE"
+    )
+
+    db.add(user)
+    db.commit()
+
+    return {
+        "message": "User created successfully"
+    }
